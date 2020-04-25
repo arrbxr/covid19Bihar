94aa3aa4c1e67cfe06efb9c6c2b72e718ce85c13
arrbxr <arr.bxr@gmail.com>
2020-04-26 04:31:23 +0530

    Updates

---
+++ precache-manifest.03483734896dcdc088c3f961d45949a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c28c7695bc52bc2217c1503ac3142ca",
+    "url": "/covid19bihar/index.html"
+  },
+  {
+    "revision": "ef594230c6cd6900c0f3",
+    "url": "/covid19bihar/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "003739f5b9de390a375b",
+    "url": "/covid19bihar/static/css/main.b3cce50d.chunk.css"
+  },
+  {
+    "revision": "ef594230c6cd6900c0f3",
+    "url": "/covid19bihar/static/js/2.20197cc7.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid19bihar/static/js/2.20197cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003739f5b9de390a375b",
+    "url": "/covid19bihar/static/js/main.98eee939.chunk.js"
+  },
+  {
+    "revision": "e1fa400f46031b052207",
+    "url": "/covid19bihar/static/js/runtime-main.831b40ae.js"
+  }
+]);
